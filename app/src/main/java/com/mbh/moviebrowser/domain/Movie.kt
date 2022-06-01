2e8af883a428f7efcbc731ef9b7a71c15ff47b91
edmund.boros <borosedmund@gmail.com>
2022-06-01 20:45:54 +0200

    Add Serialized anotation for rating and coverUrl

--- app/src/main/java/com/mbh/moviebrowser/domain/Movie.kt
+++ app/src/main/java/com/mbh/moviebrowser/domain/Movie.kt
@@ -1,9 +1,13 @@
 package com.mbh.moviebrowser.domain
 
+import com.google.gson.annotations.SerializedName
+
 data class Movie(
     val id: Long,
     val title: String,
     val overview: String,
+    @SerializedName("poster_path")
     val coverUrl: String,
+    @SerializedName("vote_average")
     val rating: Float
 )
