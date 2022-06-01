7fd8af1dbf62aa5351a0f2f0c3f3206b490c28e3
edmund.boros <borosedmund@gmail.com>
2022-06-01 20:34:54 +0200

    Add RetrofitClient

---
+++ app/src/main/java/com/mbh/moviebrowser/network/RetrofitClient.kt
@@ -0,0 +1,15 @@
+package com.mbh.moviebrowser.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object RetrofitClient {
+    val baseUrl = "https://api.themoviedb.org/"
+
+    fun getInstance(): Retrofit {
+        return Retrofit.Builder().baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
